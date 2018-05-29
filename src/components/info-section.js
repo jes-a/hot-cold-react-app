23603f02dfa7caaee4f76959633abc196ce43af4
Jes Albro <jesalbro@gmail.com>
2018-05-28 17:06:59 -0700

    Add test files

--- src/components/info-section.js
+++ src/components/info-section.js
@@ -19,21 +19,3 @@ export default function InfoSection(props) {
 }
 
 
-// export default function InfoSection(props) {
-// 	return(
-// 		<section id="what" tabIndex="-1" className="content">
-// 				<h3>What Do I Do?</h3>
-// 				<div>
-// 					<p>This is a Hot or Cold Number Guessing Game. Here are the rules: </p>
-// 					<ol className="info-ul">
-// 						<li>I pick a <strong>random secret number</strong> between 1 to 100 and keep it hidden.</li>
-// 						<li>You need to <strong>guess</strong> until you can find the hidden secret number.</li>
-// 						<li>You will <strong>get feedback</strong> on how close ("hot") or far ("cold") your guess is.</li>
-// 					</ol>
-// 					<p>If you're using a screen reader, you can use the visually hidden "Hear state of game" link in the navigation region to get a reminder of all your past guesses and how close your current guess is to being correct.</p>
-// 				</div>
-// 		</section>
-// 	);
-// }
-
-
---
+++ src/components/status-section.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import StatusSection from '../components/status-section.js';
+
+describe('<StatusSection />', () => {
+	it('Renders without crashing', () => {
+		shallow(<StatusSection auralStatus="" guesses={[]} />);
+	});
+
+});
